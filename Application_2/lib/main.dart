870c3db6a1b32dd60ec45f8b3130fcf3d756eaed
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2021-10-31 11:54:27 +0600

    apps development
    Cross platform apps development using flutter

---
+++ Application_2/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Row'),
+        ),
+        body: Row(
+          children: [
+            Text("Flutter "),
+            Text("Column"),
+            Text("Created by AR Tom"),
+            Image.network(
+              'https://images.unsplash.com/photo-1526336024174-e58f5cdd8e13?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80',
+              height: 250,
+              width: 300,
+            ),
+            Image.asset('media/tom.png', height: 250, width: 300),
+          ],
+        ),
+      ),
+    );
+  }
+}
